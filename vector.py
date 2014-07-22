d5340371a6408a7199a11082f508332b26106e9b
Jerzy Spendel <jspendel@gmail.com>
2014-07-22 17:55:53 +0000

    Class Vector moved to module vector

--- vector.py
+++ vector.py
@@ -1,5 +1,78 @@
 import sdl2, sdl2.sdlgfx
 from ctypes import *
+STANDARD_LENGTH = 1
+class Vector:
+    def __init__(self, x=None, y=None):
+        if x==None and y==None:
+            self.x, self.y = 1,0
+        else:
+            self.x, self.y = x,y
+
+    def length(self):
+        return math.sqrt(self.x**2+self.y**2)
+
+    def current_angle(self):
+        v = Vector()
+        return math.degrees(math.acos((self.x*v.x+self.y*v.y)/(self.length()*v.length())))
+
+    def reduce_me(self, to=STANDARD_LENGTH):
+        if self.length()>to:
+            sin = self.y/self.length()
+            angr = math.asin(sin)
+            sin_a = math.sin(angr)
+            cos_a = math.cos(angr)
+            if cos_a < 0:
+                if self.x > 0:
+                    self.x = -to*cos_a
+                else:
+                    self.x = to*cos_a
+            elif cos_a >= 0:
+                if self.x > 0:
+                    self.x = to*cos_a
+                else:
+                    self.x = -to*cos_a
+            if sin_a < 0:
+                if self.y > 0:
+                    self.y = -to*sin_a
+                else:
+                    self.y = to*sin_a
+            else:
+                if self.y > 0:
+                    self.y = to*sin_a
+                else:
+                    self.y = -to*sin_a
+
+    def update_to_angle(self, ang, dt):
+        angr = math.radians(ang)
+        v = (1,0)
+        xp = v[0]*math.cos(angr) - v[1]*math.sin(angr)
+        yp = v[0]*math.sin(angr) + v[1]*math.cos(angr)
+        vp = (xp*dt*3, yp*dt*3)
+        self.x += vp[0]
+        self.y += vp[1]
+        self.reduce_me()
+
+    def set_angle(self, ang):
+        l = self.length()
+        ang = math.radians(ang)
+        self.x = l*math.cos(ang)
+        self.y = l*math.sin(ang)
+
+    def imitate_resistance(self, dt):
+        l = self.length()
+        if l > 0:
+            c_p = l/STANDARD_LENGTH #Aktualny procent dlugosci
+            n_p = c_p - dt #Nowy
+            if n_p < 0:
+                self.reduce_me(0)
+                return
+            self.reduce_me(n_p*STANDARD_LENGTH)
+
+    @staticmethod
+    def angle_between(v1, v2):
+        i = v1.x*v2.x+v1.y*v2.y
+        cos = i/(v1.length()*v2.length())
+        return math.acos(cos)
 class SpeedVector:
     w = 100
     h = 100
